c3c6ff252928db4ec1b66e3df8922268f1d155c6
Gagan Bindal <gaganbindal2002@gmail.com>
2023-02-06 10:31:55 +0530

    drawer

--- lib/constants/drawer.dart
+++ lib/constants/drawer.dart
@@ -1,62 +1,35 @@
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'drawer_header.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:fitness_app/constants/navigation.dart';
 
-// class NavigationDrawer extends StatefulWidget {
-//   const NavigationDrawer({super.key});
+class Navigation_Drawer extends StatelessWidget {
+  const Navigation_Drawer({super.key});
 
-//   @override
-//   State<NavigationDrawer> createState() => _NavigationDrawerState();
-// }
+  // final CollectionReference post = FirebaseFirestore.instance.collection('Users');
 
-// class _NavigationDrawerState extends State<NavigationDrawer> {
-
-//   final CollectionReference post = FirebaseFirestore.instance.collection('Users');
-//   final name = 
-//   @override
-//   Widget build(BuildContext context) {
-//     return (Drawer(
-//       child: ListView(
-//         padding: EdgeInsets.zero,
-//         children: <Widget>[
-//           UserAccountsDrawerHeader(
-//             accountEmail: Text(name),
-//             accountName: Text(email),
-//             decoration: BoxDecoration(
-//               color: Color.fromARGB(255, 208, 214, 218),
-//             ),
-//           ),
-//           ListTile(
-//             leading: new IconButton(
-//               icon: new Icon(Icons.home, color: Colors.black),
-//               onPressed: () => null,
-//             ),
-//             title: Text('Home'),
-//             onTap: () {
-//               print(widget.uid);
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(builder: (context) => Home(uid: widget.uid)),
-//               );
-//             },
-//           ),
-//           ListTile(
-//             leading: new IconButton(
-//               icon: new Icon(Icons.settings, color: Colors.black),
-//               onPressed: () => null,
-//             ),
-//             title: Text('Settings'),
-//             onTap: () {
-//               print(widget.uid);
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(
-//                     builder: (context) => SettingsRoute(uid: widget.uid)),
-//               );
-//             },
-//           ),
-//         ],
-//       ),
-//     ));
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return 
+      Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(children: [
+              MyDrawerHeader(),
+              TextButton(onPressed: () {}, 
+              child: Row(children: [
+                Text("Logout", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800, fontFamily: "Inder"),),
+                Icon(Icons.logout_rounded,
+                size: 30,),
+              ],)),
+            ]),
+          ),
+        ),
+        
+      );
+      
+    
+    // 
+  }
+}
